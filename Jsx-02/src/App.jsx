db832cbd5867999f5692df9c8160b7d8074a8434
AnugrahaTamang <tamanganugrah59@gmail.com>
2024-11-14 21:47:09 +0545

    react jsx

---
+++ Jsx-02/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import WelcomeMessage from './WelcomeMessage'
+
+function App() {
+
+  return(
+    <>
+    <WelcomeMessage/>
+    </>
+  )
+
+}
+export default App
---
+++ Jsx-02/src/WelcomeMessage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const WelcomeMessage = () => {
+  return (
+    <div>
+        <h1>Hello world</h1>
+        <h2>Welcome to learning jsx</h2>
+    </div>
+  )
+}
+
+export default WelcomeMessage
