6abc4dc7d0ecb4798b1249f461f90f7f577bd37e
AnugrahaTamang <tamanganugrah59@gmail.com>
2024-11-14 21:32:33 +0545

    component

---
+++ Component-01/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+const App = () => {
+  return (
+   <>
+   <h1>This is the component.</h1>
+   <h2>Welcome to React basic to advance level project</h2>
+   </>
+  )
+}
+
+export default App
