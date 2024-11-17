7be45bd8b23516b971f97a21a7da64bfa5dd0d0f
AnugrahaTamang <tamanganugrah59@gmail.com>
2024-11-17 15:20:07 +0545

    list of data

---
+++ List-of-Data/src/App.jsx
@@ -0,0 +1,16 @@
+import ProductList from "./Component/ProductList"
+import Userlist from "./Component/Userlist"
+
+
+function App() {
+ 
+
+  return (
+    <>
+     <Userlist/>
+     <ProductList/>
+    </>
+  )
+}
+
+export default App
