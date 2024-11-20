4cfbdd4e85cbabd9b6a00fc3f01d92ccbc926a05
AnugrahaTamang <tamanganugrah59@gmail.com>
2024-11-20 16:45:57 +0545

    react portal

---
+++ Portral-07/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import CopyInput from './Component/CopyInput'
+
+
+const App = () => {
+  return (
+    <div>
+      <CopyInput/>
+    </div>
+  )
+}
+
+export default App
